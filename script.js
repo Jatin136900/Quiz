aee3fabb1abd81aac59b778fb157ded241025fd0
Jatin <jatinverma2428@gmail.com>
2025-05-21 13:43:25 +0530

    sir task new

---
+++ script.js
@@ -0,0 +1,63 @@
+let data = [
+    {
+        question: "What is the capital of India?",
+        answer: "New Delhi",
+        options: ["Mumbai", "New Delhi", "Kolkata", "Chennai"],
+    },
+    {
+        question: "What is the capital of Nepal?",
+        answer: "Kathmandu",
+        options: ["Kathmandu", "Pokhara", "Lalitpur", "Bharatpur"],
+    },
+    {
+        question: "What is the capital of Pakistan?",
+        answer: "Islamabad",
+        options: ["Karachi", "Rawalpindi", "Islamabad", "Multan"],
+    },
+    {
+        question: "What is 2+2+2?",
+        answer: "6",
+        options: ["222", "0", "6", "8"],
+    },
+
+
+];
+
+
+
+// let q = data[Math.floor(Math.random() * data.length)];
+
+
+// document.querySelector(".question").innerText = q.question;
+
+
+// let options = q.options.sort(() => Math.random());
+
+
+// let allOptions = document.querySelectorAll(".option");
+// for(let i = 0; i < allOptions.length; i++) {
+//     allOptions[i].innerText = options[i];
+// }
+
+
+
+
+
+printQuestion(data);
+
+function printQuestion(arr) {
+  const selectedObj = arr[getARandomValue(arr.length)];
+  const questionPara = document.querySelector(".question");
+  const options = document.querySelectorAll(".option");
+
+  questionPara.innerText = selectedObj.question;
+  options.forEach(
+    (option, index) => (option.innerText = selectedObj.options[index])
+  );
+}
+
+function getARandomValue(len) {
+  return Math.floor(Math.random() * len);
+}
+
+
