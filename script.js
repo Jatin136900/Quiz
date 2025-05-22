a24de3ec33c09c31569f06c7e5cd03d61c8b7512
Jatin <jatinverma2428@gmail.com>
2025-05-22 11:55:08 +0530

    sir task new

--- script.js
+++ script.js
@@ -43,21 +43,60 @@ let data = [
 
 
 
-printQuestion(data);
 
-function printQuestion(arr) {
-  const selectedObj = arr[getARandomValue(arr.length)];
-  const questionPara = document.querySelector(".question");
-  const options = document.querySelectorAll(".option");
 
-  questionPara.innerText = selectedObj.question;
-  options.forEach(
-    (option, index) => (option.innerText = selectedObj.options[index])
-  );
+
+
+//   printQuestion(data);
+
+
+// function printQuestion(arr) {
+//   const selectedObj = arr[getARandomValue(arr.length)];
+//   const questionPara = document.querySelector(".question");
+//   const options = document.querySelectorAll(".option");
+
+//   questionPara.innerText = selectedObj.question;
+//   options.forEach(
+//     (option, index) => (option.innerText = selectedObj.options[index])
+//   );
+// }
+
+// function getARandomValue(len) {
+//   return Math.floor(Math.random() * len);
+// }
+
+
+
+
+//   setTimeout(() => {
+//   printQuestion(data);
+// }, 1000);
+
+
+
+
+
+
+
+setInterval(() => {
+   
+const questionElem = document.getElementById("question");
+const optionsElems = document.querySelectorAll(".option");
+const answerElem = document.getElementById("answer");
+
+function displayQuestion() {
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const currentQuestion = data[randomIndex];
+
+  questionElem.textContent = currentQuestion.question;
+
+  optionsElems.forEach((optionElem, index) => {
+    optionElem.textContent = currentQuestion.options[index];
+  });
+
+
+  answerElem.textContent = "Answer: " + currentQuestion.answer;
 }
 
-function getARandomValue(len) {
-  return Math.floor(Math.random() * len);
-}
-
-
+displayQuestion();
+}, 2000);
